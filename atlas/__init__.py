144f37403601631fbba7d6d1b131e828e19e7289
Daniele Barile <daniele.barile+amplifon@cons.amplifon.com>
2025-03-18 17:38:25 +0100

    Add openFile function import to enhance file handling capabilities

--- atlas/__init__.py
+++ atlas/__init__.py
@@ -1,7 +1,7 @@
 from .core import groqPrompt, functionCall
 from .tts import speak
 from .screenshot import takeScreenshot, visionPrompt
-from .fileHandler import handleFileSearchPrompt, handleFileChoice
+from .fileHandler import handleFileSearchPrompt, handleFileChoice, openFile
 from .weather import handleWeatherPrompt
 from .wakeword import startListening
 from .audioProcessing import waveToText, extractPrompt
